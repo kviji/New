44f43eb955b29789b10ba2136f66bbe741a4e629
kviji <vijisweety82@gmail.com>
2014-02-25 14:45:06 +0530

    Toast test ptoject added

---
+++ ToastTest/src/com/example/toasttest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.toasttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private transient  Toast toast = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		final ViewGroup parent=(ViewGroup) findViewById(android.R.id.content);
+		toast = new Toast(getApplicationContext());
+		toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+		toast.setDuration(Toast.LENGTH_LONG);
+		
+		final LayoutInflater inflater =(LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);  
+		final View view = inflater.inflate(R.layout.toast,parent,false );
+		toast.setView(view);
+		
+	}
+ 
+	public void onClick(final View view) {
+		toast.show();
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
